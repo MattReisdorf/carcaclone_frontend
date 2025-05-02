93a6282923a4c92daa56a02f2ee5fb5b49e1bf8d
Matt Reisdorf <mreisdorf9717@gmail.com>
2025-05-02 12:25:50 -0400

    frontend setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  define: {
+    global: 'globalThis'
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  }
 })
