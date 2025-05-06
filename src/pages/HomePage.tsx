5cf2e0d480357b478579df1a0cae53211e22d26a
Matt Reisdorf <mreisdorf9717@gmail.com>
2025-05-06 00:25:09 -0400

    new landing page layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,13 +7,16 @@ import { Lobby } from "../interfaces/LobbyInterfaces";
 // Importing logo from assets
 import logo from "../assets/carcaclone.png";
 
+import placeholder from "../assets/art-placeholder.jpg";
+
+import background from "../assets/landing-background-v1.png";
+
 // For Generating Player Names
 import {
   uniqueNamesGenerator,
   names,
   NumberDictionary,
 } from "unique-names-generator";
-
 
 // Generate Player's UUID
 const generateUUID = () => {
@@ -55,7 +58,7 @@ const HomePage: React.FC = () => {
     const stompClient = new Client({
       brokerURL: undefined,
       webSocketFactory: () => new SockJS("http://localhost:8080/ws"),
-      reconnectDelay: 5000
+      reconnectDelay: 5000,
     });
     stompClient.onConnect = () => {
       stompClient.subscribe("/topic/lobby", (message) => {
@@ -63,107 +66,95 @@ const HomePage: React.FC = () => {
           const lobby: Lobby = JSON.parse(message.body);
           console.log(lobby);
           if (lobby.lobbyId) {
-            navigate(`/lobby/${lobby.lobbyId}`)
+            navigate(`/lobby/${lobby.lobbyId}`);
           }
         } catch (error) {
           console.error("Error parsing lobby creation message: ", error);
         }
-      })
-    }
+      });
+    };
     stompClient.activate();
     console.log("Stomp Client Activated");
     clientRef.current = stompClient;
-    console.log("Client Ref Set To: ", clientRef.current)
+    console.log("Client Ref Set To: ", clientRef.current);
 
     return () => {
       stompClient.deactivate();
       console.log("Stomp Client Deactivated");
-    }
+    };
   }, []);
 
   // Create Lobby Handler
   const createLobby = () => {
-    console.log("Create Lobby Handler")
+    console.log("Create Lobby Handler");
     clientRef.current?.publish({
       destination: "/app/createLobby",
       body: JSON.stringify({
         action: "createLobby",
         playerId: playerId,
-        playerName: playerName
-      })
-    })
-  }
+        playerName: playerName,
+      }),
+    });
+  };
 
   return (
-    <div className="bg-mirage-500 flex h-screen w-full">
-      <div className="grid h-full w-full grid-cols-1 grid-rows-[80px_repeat(6,_minmax(0,_1fr))_40px] gap-4 p-4 md:grid-cols-2">
-        <div className="row-span-1 flex h-full w-full items-center justify-center md:col-span-2">
-          <img src={logo} alt="Carcaclone" style={{ height: 64, width: 205 }} />
-        </div>
-
-        <div className="row-span-2 flex h-full w-full items-center justify-center md:row-span-6">
-          <div className="border-alabaster-500 aspect-square h-full border md:h-[65%] md:max-h-full md:w-auto md:max-w-[90%]">
-            {/* Artwork Placeholder */}
-            <div className="text-alabaster-500 flex h-full w-full items-center justify-center">
-              Artwork
+    <div
+      className="bg-mirage-500 h-screen w-full bg-cover bg-center p-4"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className="flex h-full w-full flex-col gap-4">
+        <header className="h-[80px] w-full flex items-center justify-center">
+          <img src = {logo} alt = "Carcaclone" style = {{ height: 64, width: 205 }} />
+        </header>
+        <div className="flex w-full flex-1 flex-col items-center justify-center">
+          <div className="flex h-[400px] w-[345px] flex-col gap-4">
+            <div className="flex h-1/3 w-full items-center justify-center">
+              <h1 className="font-roboto text-alabaster-500 text-center text-2xl font-semibold">
+                Free Online Carcassonne
+              </h1>
             </div>
-          </div>
-        </div>
 
-        <div className="row-span-4 flex h-full w-full items-center justify-center md:row-span-6">
-          <div className="aspect-square h-full md:h-[65%] md:max-h-full md:w-auto md:max-w-[90%]">
-            <div className="grid h-full w-full grid-cols-1 grid-rows-4 gap-4 md:max-h-[350px]">
+            <div className="grid h-2/3 w-full grid-cols-1 grid-rows-3 gap-4">
               <div className="row-span-1 h-full w-full">
-                <h1 className="font-roboto text-alabaster-500 pt-4 text-center text-xl md:text-xl">
-                  Free Online Carcassonne Alternative
-                </h1>
-              </div>
-
-              <div className="md:max-h- row-span-3 grid h-full w-full grid-cols-1 grid-rows-3 gap-4 p-4">
-                <div className="h-full w-full">
-                  <Link to="/how-to-play">
-                    <button className="bg-supernova-500 text-mirage-500 h-full w-full transform cursor-pointer rounded-lg border border-transparent font-semibold hover:scale-102 hover:border">
-                      How to Play
-                    </button>
-                  </Link>
-                </div>
-
-                <div className="h-full w-full">
-                  <button
-                    className="bg-picton-blue-500 text-mirage-500 h-full w-full transform cursor-pointer rounded-lg border border-transparent font-semibold hover:scale-102 hover:border"
-                    onClick={createLobby}
-                  >
-                    Create Lobby
+                <Link to="/how-to-play">
+                  <button className="bg-supernova-500 text-mirage-500 h-full w-full transform cursor-pointer rounded-lg border border-transparent font-semibold hover:scale-102 hover:border">
+                    How to Play
                   </button>
-                </div>
-
-                <div className="h-full w-full">
-                  <Link to="/lobbies">
-                    <button className="bg-picton-blue-500 text-mirage-500 h-full w-full transform cursor-pointer rounded-lg border border-transparent py-3 font-semibold hover:scale-102 hover:border">
-                      Join Lobby
-                    </button>
-                  </Link>
-                </div>
+                </Link>
+              </div>
+              <div className="row-span-1 h-full w-full">
+                <button
+                  className="bg-picton-blue-500 text-mirage-500 h-full w-full transform cursor-pointer rounded-lg border border-transparent font-semibold hover:scale-102 hover:border"
+                  onClick={createLobby}
+                >
+                  Create Lobby
+                </button>
+              </div>
+              <div className="row-span-1 h-full w-full">
+                <Link to="/lobbies">
+                  <button className="bg-picton-blue-500 text-mirage-500 h-full w-full transform cursor-pointer rounded-lg border border-transparent py-3 font-semibold hover:scale-102 hover:border">
+                    Join Lobby
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
         </div>
-
-        <div className="row-span-1 grid h-full w-full grid-cols-5 grid-rows-1 md:col-span-2">
-          <div className="col-span-1 flex h-full w-full items-center justify-start">
-            <h1 className="text-alabaster-500 font-roboto pl-2 text-sm">
-              v0.1
-            </h1>
-          </div>
-          <div className="col-span-3 flex h-full w-full items-center justify-center">
-            <h1 className="text-alabaster-500 font-roboto text-center text-sm">
-              Made by Matt Reisdorf
-            </h1>
-          </div>
-          <div className="col-span-1 flex h-full w-full items-center justify-end">
-            <h1 className="text-alabaster-500 font-roboto pr-2 text-sm">GH</h1>
-          </div>
-        </div>
+        <footer className="h-[80px] w-full grid grid-cols-5 grid-rows-1">
+          <div className="col-span-1 flex h-full w-full items-end justify-start">
+              <h1 className="text-alabaster-500 font-roboto pl-2 text-sm">
+                v0.1
+              </h1>
+            </div>
+            <div className="col-span-3 flex h-full w-full items-end justify-center">
+              <h1 className="text-alabaster-500 font-roboto text-center text-sm">
+                Made by Matt Reisdorf
+              </h1>
+            </div>
+            <div className="col-span-1 flex h-full w-full items-end justify-end">
+              <h1 className="text-alabaster-500 font-roboto pr-2 text-sm">GH</h1>
+            </div>
+        </footer>
       </div>
     </div>
   );
