e20ad9e1c3de2d484400ef88eedbe605826fb275
Matt Reisdorf <mreisdorf9717@gmail.com>
2025-05-08 01:02:40 -0400

    added lobby page with lobbyId parameter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LobbyList from "./pages/LobbyList";
+import LobbyPage from "./pages/LobbyPage";
 
 const App: React.FC = () => {
   return (
@@ -8,7 +9,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         {/* <Route path = "how-to-play" element = {<HowToPlay />} /> */}
-        <Route path = "lobbies" element = {<LobbyList />} />
+        <Route path = "/lobbies" element = {<LobbyList />} />
+        <Route path = "/lobby/:lobbyId" element = {<LobbyPage />} />
       </Routes>
     </Router>
   );
